113d395a4b2b79ddff56ecb430812c025639a15a
Chris Williams <cwilliams88@gmail.com>
2016-03-19 14:45:41 -0500

    initialcommit

---
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.android.justjava;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        display(9001);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+}
