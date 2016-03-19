2d591c73553d489f23925439e1c2692f033c1848
Chris Williams <cwilliams88@gmail.com>
2016-03-19 15:33:47 -0500

    pseudo-increment/decrement

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -14,12 +14,33 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    /**
+     * Increase number of coffees to order by 1
+     */
+    public void increment(View view){
+        int quantity = 3;
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + quantity);
+    }
+
+    /**
+     * Decrease number of coffees to order by 1
+     */
+    public void decrement(View view){
+        int quantity = 1;
+        TextView quantityTextView = (TextView) findViewById(
+                R.id.quantity_text_view);
+        quantityTextView.setText("" + quantity);
+    }
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        display(2);
-        displayPrice(2*5);
+        int quantity = 3;
+        display(quantity);
+        displayPrice(quantity*5);
     }
 
     /**
