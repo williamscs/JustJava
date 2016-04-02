a19f985656dd5d2995d2200e249a1e18bd890727
Chris Williams <cwilliams88@gmail.com>
2016-04-02 16:32:21 -0500

    Add bounds checking and send email rather than output message.

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,11 +1,15 @@
 package com.example.android.justjava;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +26,11 @@ public class MainActivity extends AppCompatActivity {
      * Increase number of coffees to order by 1
      */
     public void increment(View view){
-        this.quantity++;
+        if(this.quantity < 100){
+            this.quantity++;
+        } else {
+            this.popUp("You cannot order more than 100 coffees.");
+        }
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
         quantityTextView.setText("" + quantity);
@@ -32,19 +40,22 @@ public class MainActivity extends AppCompatActivity {
      * Decrease number of coffees to order by 1
      */
     public void decrement(View view){
-        this.quantity--;
+        if(this.quantity > 1){
+            this.quantity--;
+        } else {
+            this.popUp("You cannot order fewer than 1 coffee.");
+        }
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
         quantityTextView.setText("" + quantity);
     }
-    /**
-     * This method is called when the order button is clicked.
-     */
-    public void submitOrder(View view) {
-        String summary = this.createOrderSummary(this.calculatePrice());
 
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(summary);
+    private void popUp(String message){
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, message, duration);
+        toast.show();
     }
 
     /**
@@ -74,7 +85,8 @@ public class MainActivity extends AppCompatActivity {
     /**
      * Creates a custom summary message
      */
-    private String createOrderSummary(int price){
+    public void sendOrderSummary(View v){
+        int price = this.calculatePrice();
         String message = "";
         EditText et = (EditText)findViewById(R.id.name_field);
         String name = et.getText().toString();
@@ -84,6 +96,18 @@ public class MainActivity extends AppCompatActivity {
         message += "Quantity: " + this.quantity + "\n";
         message += "Total: $" + price + "\n";
         message += "Thank you!";
-        return message;
+        //return message;
+        String subject = getResources().getString(R.string.app_name);
+        this.composeEmail(subject, message);
+    }
+
+    private void composeEmail(String subject, String message) {
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 }
